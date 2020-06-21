92947626551d7b425017189fde8562394ab7c97c
bryanbain <baintrain81@att.net>
2020-06-21 16:00:18 -0400

    added Binomial_Distribution.py code

---
+++ Python_Code/Binomial_Distribution.py
@@ -0,0 +1,20 @@
+"""
+Purpose: Calculate binomial distribution values.
+Author: Bryan Bain
+Date: June 21 2020
+File: Binomial_Distribution.py
+"""
+
+import scipy.stats as stats
+
+num_trials = 10     # number of trials
+p = 0.20            # probability of success
+dp = 6              # decimal places to round to
+
+print("x".center(6) + "P(X=x)".center(12) + "P(X <= x)".center(12) +
+      "P(X >= x)".center(12))
+for i in range(num_trials+1):
+    print(f'{i}'.center(6) +
+          f'{round(stats.binom.pmf(i,num_trials,p),dp)}'.center(12) +
+          f'{round(stats.binom.cdf(i,num_trials,p),dp)}'.center(12) +
+          f'{round(1 - stats.binom.cdf(i,num_trials,p),dp)}'.center(12))
