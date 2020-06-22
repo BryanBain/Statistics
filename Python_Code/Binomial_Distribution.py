0bed6ff3479b85b0fd213984409698398673ad23
Bryan Bain <BryanBain@users.noreply.github.com>
2020-06-21 20:45:56 -0400

    Update Binomial_Distribution.py

--- Python_Code/Binomial_Distribution.py
+++ Python_Code/Binomial_Distribution.py
@@ -9,7 +9,7 @@ import scipy.stats as stats
 
 num_trials = 10     # number of trials
 p = 0.20            # probability of success
-dp = 6              # decimal places to round to
+dp = 8              # decimal places to round to
 
 print("x".center(6) + "P(X=x)".center(12) + "P(X <= x)".center(12) +
       "P(X >= x)".center(12))
@@ -17,4 +17,4 @@ for i in range(num_trials+1):
     print(f'{i}'.center(6) +
           f'{round(stats.binom.pmf(i,num_trials,p),dp)}'.center(12) +
           f'{round(stats.binom.cdf(i,num_trials,p),dp)}'.center(12) +
-          f'{round(1 - stats.binom.cdf(i,num_trials,p),dp)}'.center(12))
+          f'{round(1 - stats.binom.cdf(i-1,num_trials,p),dp)}'.center(12))
