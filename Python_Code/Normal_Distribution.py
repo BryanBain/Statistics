8b003c4e8546dc38c8bd14a7c5c68dc27e3c5880
bryanbain <baintrain81@att.net>
2020-06-25 15:19:50 -0400

    finished Normal_Distribution.py

--- Python_Code/Normal_Distribution.py
+++ Python_Code/Normal_Distribution.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Wed Jun 24 19:41:15 2020
-
-@author: bryanbain
+Purpose: Calculate cumulative and inverse cumulative normal probability
+distributions.
+Date: Jun 24, 2020
+Author: Bryan Bain
+File Name: Normal_Distribution.py
 """
 
 from scipy.stats import norm
@@ -20,7 +20,8 @@ def areaOrValue():
     """
     Prints a selection menu for the user to choose the problem type.
     """
-    print("\n1. You are trying to find area/probability/percent/etc.")
+    print("\nPlease make your selection below.")
+    print("1. You are trying to find area/probability/percent/etc.")
     print("2. You are given an area and need to find a value.")
 
 def areaPrintout():
@@ -110,21 +111,25 @@ while quit is not True:
             if z_option == "1":  # Finding area to the left of z
                 z_score = getZ()
                 area = norm.cdf(z_score)
+                print()
                 print(f"The area to the left of z = {z_score} is {area:0.4f}")
             elif z_option == "2":  # Finding area to the right of z
                 z_score = getZ()
                 area = norm.sf(z_score)
+                print()
                 print(f"The area to the right of z = {z_score} is {area:0.4f}")  
             elif z_option == "3":  # Finding area between z scores
                 z_score1 = getLowerZ()
                 z_score2 = getUpperZ()
                 area = areaBetween(z_score2, z_score1)
+                print()
                 print(f"The area between z = {z_score1} and z = {z_score2} "
                       f"is {area:0.4f}")
             elif z_option == "4":  # Finding area outside two z scores
                 z_score1 = getLowerZ()
                 z_score2 = getUpperZ()
                 area = 1 - areaBetween(z_score2, z_score1)
+                print()
                 print(f"The area outside of z = {z_score1} and z = {z_score2} "
                       f"is {area:0.4f}")
                 
@@ -136,22 +141,26 @@ while quit is not True:
                 lt_area = float(input("Please enter the area (as a decimal) " 
                                       "to the left: "))
                 z_score = norm.ppf(lt_area)
+                print()
                 print(f"z = {z_score:0.3f} has an area of {lt_area} to the left")
             elif z_option == "2":
                 rt_area = float(input("Please enter the area (as a decimal) "  
                                       "to the right: "))
                 z_score = norm.isf(rt_area)
+                print()
                 print(f"z = {z_score:0.3f} has an area of {rt_area} to the right") 
             elif z_option == "3":
                 mid_area = float(input("Please enter the area (as a decimal) "
                                        "in the middle: "))
                 z_scores = norm.interval(mid_area)
+                print()
                 print(f"The z scores are z = {z_scores[0]:0.3f} and "
                       f"z = {z_scores[1]:0.3f}")
             elif z_option == "4":
                 out_area = float(input("Please enter the total area "
                                        "(as a decimal) outside the middle: "))
                 z_scores = norm.interval(1 - out_area)
+                print()
                 print(f"The z scores are z = {z_scores[0]:0.3f} and "
                       f"z = {z_scores[1]:0.3f}")
             
@@ -159,63 +168,7 @@ while quit is not True:
         areaOrValue()
         aOrV = input()
         print()
-        if aOrV == "2":  # Trying to find observed value given an area
-            obs_value = float(input("Please enter the observed value: "))
-            mu = float(input("Please enter the population mean: "))
-            sigma = float(input("Please enter the standard deviation: "))
-            # z_score = obsToZ(obs_value, mu, sigma)
-            invAreaPrintout()
-            x_option = input().lower()
-            if x_option == "1":
-                lt_area = float(input("Please enter the area (as a decimal) "
-                                      "to the left: "))
-                obs_value = norm.ppf(lt_area, mu, sigma)
-                print(f"x = {obs_value:0.3f} has an area of "
-                      f"{lt_area} to the left")
-            elif x_option == "2":
-                rt_area = float(input("Please enter the area (as a decimal) "
-                                      "to the right: "))
-                obs_value = norm.isf(rt_area, mu, sigma)
-                print(f"z = {obs_value:0.3f} has an area of "
-                      f"{rt_area} to the right") 
-            elif x_option == "3":
-                mid_area = float(input("Please enter the area (as a decimal) "
-                                        "in the middle: "))
-                obs_values = norm.interval(mid_area, mu, sigma)
-                print(f"The observed values are x = {obs_values[0]:0.3f}"
-                      f" and x = {obs_values[1]:0.3f}")
-            elif x_option == "4":
-                out_area = float(input("Please enter the total area "
-                                        "(as a decimal) outside the middle: "))
-                obs_values = norm.interval(1 - out_area, mu, sigma)
-                print(f"The observed values are x = {obs_values[0]:0.3f} "
-                      f"and x = {obs_values[1]:0.3f}")
-            # invAreaPrintout()
-            # z_option = input().lower()
-            # if z_option == "1":
-            #     lt_area = float(input("Please enter the area (as a decimal) " 
-            #                           "to the left: "))
-            #     z_score = norm.ppf(lt_area)
-            #     print(f"z = {z_score:0.3f} has an area of {lt_area} to the left")
-            # elif z_option == "2":
-            #     rt_area = float(input("Please enter the area (as a decimal) "  
-            #                           "to the right: "))
-            #     z_score = norm.isf(rt_area)
-            #     print(f"z = {z_score:0.3f} has an area of {rt_area} to the right") 
-            # elif z_option == "3":
-            #     mid_area = float(input("Please enter the area (as a decimal) "
-            #                            "in the middle: "))
-            #     z_scores = norm.interval(mid_area)
-            #     print(f"The z scores are z = {z_scores[0]:0.3f} and "
-            #           f"z = {z_scores[1]:0.3f}")
-            # elif z_option == "4":
-            #     out_area = float(input("Please enter the total area "
-            #                            "(as a decimal) outside the middle: "))
-            #     z_scores = norm.interval(1 - out_area)
-            #     print(f"The z scores are z = {z_scores[0]:0.3f} and "
-            #           f"z = {z_scores[1]:0.3f}")
-            
-        elif aOrV == "1":  # Trying to find area
+        if aOrV == "1":  # Trying to find area
             areaPrintout()
             x_option = input().lower()
             print()
@@ -225,6 +178,7 @@ while quit is not True:
                 sigma = float(input("Please enter the standard deviation: "))
                 z_score = obsToZ(ob_val, mu, sigma)
                 area = norm.cdf(z_score)
+                print()
                 print(f"The area to the left of x = {ob_val} is {area:0.4f}")
             elif x_option == "2":
                 ob_val = float(input("Please enter the observed value: "))
@@ -232,6 +186,7 @@ while quit is not True:
                 sigma = float(input("Please enter the standard deviation: "))
                 z_score = obsToZ(ob_val, mu, sigma)
                 area = norm.sf(z_score)
+                print()
                 print(f"The area to the right of x = {ob_val} is {area:0.4f}")  
             elif x_option == "3":
                 ob_val1 = float(input("Please enter the smaller observed value: "))
@@ -241,6 +196,7 @@ while quit is not True:
                 z1 = obsToZ(ob_val1, mu, sigma)
                 z2 = obsToZ(ob_val2, mu, sigma)
                 area = norm.cdf(z2) - norm.cdf(z1)
+                print()
                 print(f"The area between x = {ob_val1} and x = {ob_val2} "
                       f"is {area:0.4f}")
             elif x_option == "4":
@@ -251,64 +207,42 @@ while quit is not True:
                 z1 = obsToZ(ob_val1, mu, sigma)
                 z2 = obsToZ(ob_val2, mu, sigma)
                 area = 1 - (norm.cdf(z2) - norm.cdf(z1))
+                print()
                 print(f"The area outside of x = {ob_val1} and x = {ob_val2} "
-                      f"is {area:0.4f}")
-    
-    # find z score or observed value for a given area
-    # elif z_or_obs == "1":
-    #     invAreaPrintout()
-    #     z_option = input().lower()
-    #     if z_option == "1":
-    #         lt_area = float(input("Please enter the area (as a decimal) " 
-    #                               "to the left: "))
-    #         z_score = norm.ppf(lt_area)
-    #         print(f"z = {z_score:0.3f} has an area of {lt_area} to the left")
-    #     elif z_option == "2":
-    #         rt_area = float(input("Please enter the area (as a decimal) "  
-    #                               "to the right: "))
-    #         z_score = norm.isf(rt_area)
-    #         print(f"z = {z_score:0.3f} has an area of {rt_area} to the right") 
-    #     elif z_option == "3":
-    #         mid_area = float(input("Please enter the area (as a decimal) "
-    #                                "in the middle: "))
-    #         z_scores = norm.interval(mid_area)
-    #         print(f"The z scores are z = {z_scores[0]:0.3f} and "
-    #               f"z = {z_scores[1]:0.3f}")
-    #     elif z_option == "4":
-    #         out_area = float(input("Please enter the total area "
-    #                                "(as a decimal) outside the middle: "))
-    #         z_scores = norm.interval(1 - out_area)
-    #         print(f"The z scores are z = {z_scores[0]:0.3f} and "
-    #               f"z = {z_scores[1]:0.3f}")
-            
-    # elif z_or_obs == "2":
-    #     # obs_value = float(input("Please enter the observed value: "))
-    #     mu = float(input("Please enter the population mean: "))
-    #     sigma = float(input("Please enter the standard deviation: "))
-    #     # z_score = obsToZ(obs_value, mu, sigma)
-    #     invAreaPrintout()
-    #     x_option = input().lower()
-    #     if x_option == "1":
-    #         lt_area = float(input("Please enter the area (as a decimal) "
-    #                               "to the left: "))
-    #         obs_value = norm.ppf(lt_area, mu, sigma)
-    #         print(f"x = {obs_value:0.3f} has an area of "
-    #               f"{lt_area} to the left")
-    #     elif x_option == "2":
-    #         rt_area = float(input("Please enter the area (as a decimal) "
-    #                               "to the right: "))
-    #         obs_value = norm.isf(rt_area, mu, sigma)
-    #         print(f"z = {obs_value:0.3f} has an area of "
-    #               f"{rt_area} to the right") 
-    #     elif x_option == "3":
-    #         mid_area = float(input("Please enter the area (as a decimal) "
-    #                                "in the middle: "))
-    #         obs_values = norm.interval(mid_area, mu, sigma)
-    #         print(f"The observed values are x = {obs_values[0]:0.3f}"
-    #               f" and x = {obs_values[1]:0.3f}")
-    #     elif x_option == "4":
-    #         out_area = float(input("Please enter the total area "
-    #                                "(as a decimal) outside the middle: "))
-    #         obs_values = norm.interval(1 - out_area, mu, sigma)
-    #         print(f"The observed values are x = {obs_values[0]:0.3f} "
-    #               f"and x = {obs_values[1]:0.3f}")
+                      f"is {area:0.4f}")      
+                
+        elif aOrV == "2":  # Trying to find observed value given an area
+            mu = float(input("Please enter the population mean: "))
+            sigma = float(input("Please enter the standard deviation: "))
+            invAreaPrintout()
+            x_option = input().lower()
+            if x_option == "1":
+                lt_area = float(input("Please enter the area (as a decimal) "
+                                      "to the left: "))
+                obs_value = norm.ppf(lt_area, mu, sigma)
+                print()
+                print(f"x = {obs_value:0.3f} has an area of "
+                      f"{lt_area} to the left")
+            elif x_option == "2":
+                rt_area = float(input("Please enter the area (as a decimal) "
+                                      "to the right: "))
+                obs_value = norm.isf(rt_area, mu, sigma)
+                print()
+                print(f"z = {obs_value:0.3f} has an area of "
+                      f"{rt_area} to the right") 
+            elif x_option == "3":
+                mid_area = float(input("Please enter the area (as a decimal) "
+                                        "in the middle: "))
+                obs_values = norm.interval(mid_area, mu, sigma)
+                print()
+                print(f"The observed values are x = {obs_values[0]:0.3f}"
+                      f" and x = {obs_values[1]:0.3f}")
+            elif x_option == "4":
+                out_area = float(input("Please enter the total area "
+                                        "(as a decimal) outside the middle: "))
+                obs_values = norm.interval(1 - out_area, mu, sigma)
+                print()
+                print(f"The observed values are x = {obs_values[0]:0.3f} "
+                      f"and x = {obs_values[1]:0.3f}")
+
+
