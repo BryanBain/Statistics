adab17784ce75f466b2421fc8013dbb096ec07fc
bryanbain <baintrain81@att.net>
2020-06-25 08:32:29 -0400

    need to refactor Normal__Distribution.py

--- Python_Code/Normal_Distribution.py
+++ Python_Code/Normal_Distribution.py
@@ -8,6 +8,15 @@ Created on Wed Jun 24 19:41:15 2020
 
 from scipy.stats import norm
 
+def obsToZ(x, mu, sigma):
+    """
+    Converts an observed value to its z score.
+    x: the observed value
+    mu: the mean
+    sigma: the standard deviation
+    """
+    return (x - mu) / sigma
+
 # Find area/prob/percent/etc
 
 print("Are you given a z score or an observed value?: ")
@@ -77,4 +86,65 @@ elif z_or_obs == "2":
         area = 1 - (norm.cdf(obs_value2, mu, sigma) - norm.cdf(obs_value1, mu, sigma))
         print(f"The area outside of x = {obs_value1} and x = {obs_value2} "
               f"is {area:0.4f}")
+
+
 # Find observed value
+
+print("Are you finding a z score or an observed value?: ")
+print("1. z score")
+print("2. observed value")
+z_or_obs = input()
+
+if z_or_obs == "1":
+    print("Please select the area you are given.\n")
+    print("1. Area to the left")
+    print("2. Area to the right")
+    print("3. Middle area two z scores")
+    print("4. Area outside of two middle z scores")
+    z_option = input()
+    if z_option == "1":
+        lt_area = float(input("Please enter the area (as a decimal) to the left: "))
+        z_score = norm.ppf(lt_area)
+        print(f"z = {z_score:0.3f} has an area of {lt_area} to the left")
+    elif z_option == "2":
+        rt_area = float(input("Please enter the area (as a decimal) to the right: "))
+        z_score = norm.isf(rt_area)
+        print(f"z = {z_score:0.3f} has an area of {rt_area} to the right") 
+    elif z_option == "3":
+        mid_area = float(input("Please enter the area (as a decimal) in the middle: "))
+        z_scores = norm.interval(mid_area)
+        print(f"The z scores are z = {z_scores[0]:0.3f} and z = {z_scores[1]:0.3f}")
+    elif z_option == "4":
+        out_area = float(input("Please enter the total area (as a decimal) outside the middle: "))
+        z_scores = norm.interval(1 - out_area)
+        print(f"The z scores are z = {z_scores[0]:0.3f} and z = {z_scores[1]:0.3f}")
+        
+elif z_or_obs == "2":
+    # obs_value = float(input("Please enter the observed value: "))
+    mu = float(input("Please enter the population mean: "))
+    sigma = float(input("Please enter the standard deviation: "))
+    # z_score = obsToZ(obs_value, mu, sigma)
+    print("Please select the area you are given.\n")
+    print("1. Area to the left")
+    print("2. Area to the right")
+    print("3. Middle area two z scores")
+    print("4. Area outside of two middle z scores")
+    x_option = input()
+    if x_option == "1":
+        lt_area = float(input("Please enter the area (as a decimal) to the left: "))
+        obs_value = norm.ppf(lt_area, mu, sigma)
+        print(f"x = {obs_value:0.3f} has an area of {lt_area} to the left")
+    elif x_option == "2":
+        rt_area = float(input("Please enter the area (as a decimal) to the right: "))
+        obs_value = norm.isf(rt_area, mu, sigma)
+        print(f"z = {obs_value:0.3f} has an area of {rt_area} to the right") 
+    elif x_option == "3":
+        mid_area = float(input("Please enter the area (as a decimal) in the middle: "))
+        obs_values = norm.interval(mid_area, mu, sigma)
+        print(f"The observed values are x = {obs_values[0]:0.3f}"
+              f" and x = {obs_values[1]:0.3f}")
+    elif x_option == "4":
+        out_area = float(input("Please enter the total area (as a decimal) outside the middle: "))
+        obs_values = norm.interval(1 - out_area, mu, sigma)
+        print(f"The observed values are x = {obs_values[0]:0.3f} "
+              f"and x = {obs_values[1]:0.3f}")
