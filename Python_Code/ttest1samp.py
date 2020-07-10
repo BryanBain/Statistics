6a0fbc9ba00826b58f011b140710147f1853a8db
Bryan Bain <BryanBain@users.noreply.github.com>
2020-07-10 18:44:23 -0400

    Update ttest1samp.py
    Added the ability for the user to quit the program, added comments, and fixed some bugs.

--- Python_Code/ttest1samp.py
+++ Python_Code/ttest1samp.py
@@ -12,49 +12,81 @@ import scipy.stats as stats
 import math
 import numpy as np
 
-def obsToT(xbar, mu, sigma, n):  # convert raw data to t statistic
+def obsToT(xbar, mu, sigma, n):
     return (xbar-mu)/(sigma/math.sqrt(n))
 
-mu = float(input("Please enter the original (claimed) mean: "))
-significance = eval(input("Please enter the significance level: "))
-print()
+quit = False
 
-print("Are you given data or summary statitiscs? ")
-print("1. Data set")
-print("2. Summary statistics")
-
-data_or_sum = input()
-if data_or_sum == "1":  # user enters data set
-    done = False
-    dataset = []
-    print("Please enter the data set. Press 'q' when done.")
-    while not done:
-        element = input()
-        if element == 'q':
-            break
-        element = float(element)
-        dataset.append(element)
-    xbar = np.mean(dataset)
-    std_dev = np.std(dataset, ddof=1)
-    sample_size = len(dataset)
+while not quit:
+    
+    mu = input("Please enter the original (claimed) mean: ")
+    if mu == 'q':  # quit the program
+        quit = True
+        break
+    significance = input("Please enter the significance level, as a decimal: ")
+    if significance == 'q':  # quit the program
+        quit = True
+        break
+    mu = float(mu)
+    significance = float(significance)
     print()
-    print(f"Sample mean: {xbar:0.4f}")
-    print(f"Sample standard deviation: {std_dev:0.4f}")
-    print(f"Sample size: {sample_size:0.0f}")
 
-else:  # user enters summary statistics
-    xbar = float(input("Please enter the sample mean: "))
-    std_dev = float(input("Please enter the sample standard deviation: "))
-    sample_size = int(input("Please enter the sample size: "))
-print()
+    print("Are you given data or summary statitiscs? ")
+    print("1. Data set")
+    print("2. Summary statistics")
 
-test_statistic = obsToT(xbar, mu, std_dev, sample_size)
-df = sample_size - 1
-area = stats.t.cdf(test_statistic, df=df, loc=0, scale=1)
-ci_min = stats.t.interval(1-significance, df=df, loc=xbar, scale=std_dev/math.sqrt(sample_size))[0]
-ci_max = stats.t.interval(1-significance, df=df, loc=xbar, scale=std_dev/math.sqrt(sample_size))[1]
-print(f"Test statistic: {test_statistic:0.3f}")
-print(f"Ha < H0 p-value: {area:0.4f}")
-print(f"Ha > H0 p-value: {1-area:0.4f}")
-print(f"Ha != H0 p-value: {2*area:0.4f}")
-print(f"{100*(1-significance):.0f}% Confidence Interval: ({ci_min:0.4f}, {ci_max:0.4f})")
+    data_or_sum = input()
+    if data_or_sum == 'q':  # quit the program
+        quit = True
+    elif data_or_sum == "1":  # user will be manually entering a dataset
+        done = False
+        dataset = []
+        print("Please enter the data set. Press 'd' when done, and 'u' to undo.")
+        while not done:
+            element = input()
+            if element == 'd':  # user is done entering data values
+                break
+            elif element == 'u':  # undo the last data entry
+                dataset.pop()
+                continue
+            elif element == 'q':  # quit the program
+                quit = True
+                break
+            dataset.append(float(element))  # cast entry as a float and append to dataset
+            print(dataset)  # prints dataset for user after entering each value
+        xbar = np.mean(dataset)
+        std_dev = np.std(dataset, ddof=1)
+        sample_size = len(dataset)
+        print()
+        print(f"Sample mean: {xbar:0.4f}")
+        print(f"Sample standard deviation: {std_dev:0.4f}")
+        print(f"Sample size: {sample_size:0.0f}")
+    else:  # user will manually enter summary statistics
+        xbar = input("Please enter the sample mean: ")
+        if xbar == 'q':  # quit the program
+            quit = True
+            break
+        xbar = float(xbar)
+        std_dev = input("Please enter the sample standard deviation: ")
+        if std_dev == 'q':  # quit the program
+            quit = True
+            break
+        std_dev = float(std_dev)
+        sample_size = input("Please enter the sample size: ")
+        if sample_size == 'q':  # quit the program
+            quit = True
+            break
+        sample_size = int(sample_size)
+    print()
+
+    test_statistic = obsToT(xbar, mu, std_dev, sample_size)  # calculate test statistic t
+    df = sample_size - 1  # degrees of freedom
+    area = stats.t.cdf(test_statistic, df=df, loc=0, scale=1)  # area under curve
+    ci_min = stats.t.interval(1-significance, df=df, loc=xbar, scale=std_dev/math.sqrt(sample_size))[0]  # minimum of confidence interval
+    ci_max = stats.t.interval(1-significance, df=df, loc=xbar, scale=std_dev/math.sqrt(sample_size))[1]  # maximum of confidence interval
+    print(f"Test statistic: {test_statistic:0.3f}")
+    print(f"Ha < H0 critical value: {stats.t.ppf(significance,df):0.3f} p-value: {area:0.4f}")
+    print(f"Ha > H0 critical value: {stats.t.ppf(1-significance,df):0.3f} p-value: {1-area:0.4f}")
+    print(f"Ha != H0 critical value: +/-{math.fabs(stats.t.ppf(significance/2,df)):0.3f} p-value: {2*area:0.4f}")
+    print(f"{100*(1-significance):.0f}% Confidence Interval: ({ci_min:0.4f}, {ci_max:0.4f})")
+    print()
